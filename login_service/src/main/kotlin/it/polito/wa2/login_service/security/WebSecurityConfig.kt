8c73d8373a66cae3ac89f254ef1be7ce973677d7
francescolonardo <francescolonardo@gmail.com>
2022-07-23 20:32:10 +0200

    Remove useless code

--- login_service/src/main/kotlin/it/polito/wa2/login_service/security/WebSecurityConfig.kt
+++ login_service/src/main/kotlin/it/polito/wa2/login_service/security/WebSecurityConfig.kt
@@ -3,10 +3,8 @@ package it.polito.wa2.login_service.security
 import it.polito.wa2.login_service.services.LoginServiceImpl
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
-import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -34,9 +32,6 @@ class WebSecurityConfig: WebSecurityConfigurerAdapter() {
     }
 
     override fun configure(http: HttpSecurity) {
-        val userAuthenticationFilter = UserAuthenticationFilter(authenticationManagerBean(), jwtSecretB64Key, jwtExpirationTimeMs.toInt(), jwtHttpHeaderName)
-        userAuthenticationFilter.setFilterProcessesUrl("/user/login") // changes login path (where to post username and password)
-
         http.csrf().disable() // disable cross site request forgery
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // to use JWT instead of the default stateful session policy (cookie)
 
@@ -49,15 +44,5 @@ class WebSecurityConfig: WebSecurityConfigurerAdapter() {
             .antMatchers(HttpMethod.POST,"/user/login").permitAll()
         http.authorizeRequests()
             .anyRequest().authenticated() // allows only authenticated users to be able to access the remaining paths
-
-        // TODO: uncomment this if you want to use the form login provided by Spring,
-        // that requires a x-www-form-urlencoded body
-        //http.addFilter(userAuthenticationFilter) // authentication filter for checking the user when he tries to log in
     }
-
-    @Bean
-    override fun authenticationManagerBean(): AuthenticationManager {
-        return super.authenticationManagerBean()
-    }
-
 }
