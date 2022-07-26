272f9579dd630b6737154b369aaab85d1268f211
Ahmet Kaan Ipekoren <50779224+akipekoren@users.noreply.github.com>
2022-07-26 22:12:36 +0200

    updates on queries

--- ticket_catalogue_service/src/main/kotlin/it/polito/wa2/ticket_catalogue_service/repositories/OrderRepository.kt
+++ ticket_catalogue_service/src/main/kotlin/it/polito/wa2/ticket_catalogue_service/repositories/OrderRepository.kt
@@ -6,6 +6,7 @@ import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import java.sql.Timestamp
 import java.util.*
 
 @Repository
@@ -13,11 +14,17 @@ interface OrderRepository : CoroutineCrudRepository<Order, Long> {
     fun findByUsername(username: String): Flow<Order>
     suspend fun findByIdAndUsername(orderId: Long, username: String): Order?
 
-    @Query("SELECT all FROM Order o WHERE o.orderdate between '2022-01-01' and '2022-12-30'")
-    fun findOrdersIn2022(): Flow<Order?>?
+    @Query("SELECT all FROM Order o WHERE o.orderdate  > :start and  o.orderdate  < :end ")
+    fun findAllOrdersByDate(
+        @Param("start") start: Timestamp?,
+        @Param("end") end: Timestamp?
+    ): Flow<Order>
 
-    @Query("SELECT all FROM Order o WHERE o.orderdate > :constraint ")
-    fun findOrdersByDate(
-        @Param("constraint") start: Date?,
-    ): Flow<Order?>?
+
+    @Query("SELECT all FROM Order o WHERE o.username = :username and o.orderdate  > :start and  o.orderdate  < :end ")
+    fun findAllUserOrdersByDate(
+        @Param("start") start: Timestamp?,
+        @Param("end") end: Timestamp?,
+        username: String
+    ): Flow<Order>
 }
