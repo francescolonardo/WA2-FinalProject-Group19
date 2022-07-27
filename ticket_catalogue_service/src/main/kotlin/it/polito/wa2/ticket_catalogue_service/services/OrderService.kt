f06b75d17c0f410f2524e2e94d3dd44dff2905fb
Ahmet Kaan Ipekoren <50779224+akipekoren@users.noreply.github.com>
2022-07-27 08:42:49 +0200

    two new services for order

--- ticket_catalogue_service/src/main/kotlin/it/polito/wa2/ticket_catalogue_service/services/OrderService.kt
+++ ticket_catalogue_service/src/main/kotlin/it/polito/wa2/ticket_catalogue_service/services/OrderService.kt
@@ -3,6 +3,7 @@ package it.polito.wa2.ticket_catalogue_service.services
 import it.polito.wa2.ticket_catalogue_service.dtos.BillingInformationDTO
 import it.polito.wa2.ticket_catalogue_service.dtos.OrderDTO
 import kotlinx.coroutines.flow.Flow
+import java.sql.Timestamp
 
 interface OrderService {
     suspend fun addNewOrder(billingInformationDTO: BillingInformationDTO, username: String, authorizationHeader: String): Long?
@@ -11,4 +12,6 @@ interface OrderService {
     fun getAllOrders(): Flow<OrderDTO>
     suspend fun getAllOrdersByUserId(userId: Long, authorizationHeader: String): Flow<OrderDTO>
     fun updateOrderByTransactionInfo(transactionJson: String)
+    fun getAllOrdersByDate(start: Timestamp, end : Timestamp) : Flow<OrderDTO>
+    fun getAllUserOrdersByDate(start : Timestamp, end: Timestamp, username: String) : Flow<OrderDTO>
 }
