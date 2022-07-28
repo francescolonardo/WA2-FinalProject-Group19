d9e2eed945ec9f57b1ee6c919cc96e197437f5f5
se98100 <se98100@gmail.com>
2022-07-28 18:50:29 +0200

    Fix ticket's age restrictions check

--- ticket_catalogue_service/src/main/kotlin/it/polito/wa2/ticket_catalogue_service/dtos/UserDetailsDTO.kt
+++ ticket_catalogue_service/src/main/kotlin/it/polito/wa2/ticket_catalogue_service/dtos/UserDetailsDTO.kt
@@ -1,14 +1,13 @@
 package it.polito.wa2.ticket_catalogue_service.dtos
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import java.util.*
 
 data class UserDetailsDTO (
     @JsonProperty("username") val username: String,
     @JsonProperty("roles") val roles: Set<Role>,
-    @JsonProperty("dateOfBirth") val dateOfBirth: Date?,
+    @JsonProperty("date_of_birth") val dateOfBirth: String?,
     @JsonProperty("address") val address: String?,
-    @JsonProperty("telephoneNumber") val telephoneNumber: String?
+    @JsonProperty("telephone_number") val telephoneNumber: String?
 )
 
 enum class Role {
